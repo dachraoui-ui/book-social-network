1abdfa2e439c176a621669f1294cf78437476650
Ben othman Rayen <raydineri07@gmail.com>
2024-10-21 12:56:31 +0100

    first commit

---
+++ book-network/src/main/java/com/example/book_social_network/security/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.example.book_social_network.security;
+
+import com.example.book_social_network.user.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+        return userRepository.findByEmail(userEmail)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found" ));
+    }
+
+
+}
---
+++ book-network/src/main/java/com/example/book_social_network/user/Token.java
@@ -0,0 +1,28 @@
+package com.example.book_social_network.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Token {
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @Column(unique = true)
+    private String token;
+    private LocalDateTime createdAt;
+    private LocalDateTime expiresAt;
+    private LocalDateTime validatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false) //foreign key
+    private User user;
+}
