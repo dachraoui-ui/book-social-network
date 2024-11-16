242d0f24757ab30d3d8c15dc535ce8024838524e
ahmed dacharoui <dachraouia193@gmail.com>
2024-11-16 23:33:13 +0100

    complete the find method

--- book-network/src/main/java/com/example/book_social_network/book/BookRepository.java
+++ book-network/src/main/java/com/example/book_social_network/book/BookRepository.java
@@ -3,13 +3,14 @@ package com.example.book_social_network.book;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 
-public interface BookRepository extends JpaRepository<Book,Integer> {
+public interface BookRepository extends JpaRepository<Book,Integer>, JpaSpecificationExecutor<Book> {
     @Query("""
             SELECT book
             FROM Book book
-            where book.archived = false\s
+            where book.archived = false
             and book.shareable = true
             and book.owner.id != :userId
             """)
