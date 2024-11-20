bcc135a914c70b2d0eaf8f29bd0793e74c57ed1d
ahmed dacharoui <dachraouia193@gmail.com>
2024-11-20 23:09:37 +0100

    complete the readfile from location

--- book-network/src/main/java/com/example/book_social_network/book/BookMapper.java
+++ book-network/src/main/java/com/example/book_social_network/book/BookMapper.java
@@ -1,5 +1,6 @@
 package com.example.book_social_network.book;
 
+import com.example.book_social_network.file.FileUtils;
 import com.example.book_social_network.history.BookTransactionHistory;
 import org.springframework.stereotype.Service;
 
@@ -28,7 +29,7 @@ public class BookMapper {
                 .archived(book.isArchived())
                 .shareable(book.isShareable())
                 .owner(book.getOwner().fullName())
-                // todo add cover implement this later
+                .cover(FileUtils.readFileFromLocation(book.getBookCover()))
                 .build();
     }
 
