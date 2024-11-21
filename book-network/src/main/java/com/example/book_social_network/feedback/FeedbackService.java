3210a915b731e82e9934de4150e8141b8228b2b1
ahmed dacharoui <dachraouia193@gmail.com>
2024-11-21 10:02:03 +0100

    create the feedbackservice

--- book-network/src/main/java/com/example/book_social_network/feedback/FeedbackService.java
+++ book-network/src/main/java/com/example/book_social_network/feedback/FeedbackService.java
@@ -1,15 +1,34 @@
 package com.example.book_social_network.feedback;
 
 
+import com.example.book_social_network.book.Book;
+import com.example.book_social_network.book.BookRepository;
+import com.example.book_social_network.exception.OperationNotPermittedException;
+import com.example.book_social_network.history.BookTransactionHistory;
+import com.example.book_social_network.user.User;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
 
 public class FeedbackService {
 
+    private final BookRepository bookRepository;
     public Integer save(FeedbackRequest request, Authentication connectedUser) {
+        Book book = bookRepository.findById(request.bookId())
+                .orElseThrow(() -> new EntityNotFoundException("No book  found with the ID::" + request.bookId()));
+        if (book.isArchived() || !book.isShareable()){
+            throw new OperationNotPermittedException("You cannot give a feedback for an archived or non shareable book ");
+        }
+        User user = ((User) connectedUser.getPrincipal());
+        if (Objects.equals(book.getOwner().getId(),user.getId())){
+            throw new OperationNotPermittedException("You cannot give a feedback for your own book ");
+        }
+        Feedback feedback = feedbackMapper.toFeedback(request);
     }
 }
