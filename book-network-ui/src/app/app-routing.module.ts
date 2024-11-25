f2d02bfc39c40c52200d7e7d4fbe7fcf472cc8b7
Ben othman Rayen <raydineri07@gmail.com>
2024-11-25 03:48:53 +0100

    main page and menu

--- book-network-ui/src/app/app-routing.module.ts
+++ book-network-ui/src/app/app-routing.module.ts
@@ -4,6 +4,13 @@ import {LoginComponent} from './pages/login/login.component';
 import {RegisterComponent} from './pages/register/register.component';
 import {ActivateAccountComponent} from './pages/activate-account/activate-account.component';
 const routes: Routes = [
+  /*
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  */
   {
     path:'login',
     component: LoginComponent
@@ -15,6 +22,10 @@ const routes: Routes = [
   {
     path:'activate-account',
     component: ActivateAccountComponent
+  },
+  {
+    path: 'books',
+    loadChildren: () => import('./modules/book/book.module').then(m => m.BookModule),
   }
 ];
 
